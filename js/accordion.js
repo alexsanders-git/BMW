1c275efbf63dfbabbe1889a51ec034575692ecbc
Alex <alex.sanders@ukr.net>
2021-05-18 15:28:31 +0300

    Initial commit

---
+++ js/accordion.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const featureLinkElems = document.querySelectorAll('.feature__link');
+    const featureSubElems = document.querySelectorAll('.feature-sub');
+
+    featureLinkElems.forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            if (featureLinkElems[i].classList.contains('feature__link_active')) {
+                featureSubElems[i].classList.add('hidden');
+                featureLinkElems[i].classList.remove('feature__link_active');
+            } else {
+                featureSubElems.forEach((el) => {
+                    el.classList.add('hidden');
+                });
+                featureLinkElems.forEach((el) => {
+                    el.classList.remove('feature__link_active');
+                });
+                featureSubElems[i].classList.remove('hidden');
+                btn.classList.add('feature__link_active');
+            }
+        });
+    });
+});
