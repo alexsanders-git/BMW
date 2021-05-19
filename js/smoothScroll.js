17edde8f89f997f6dd1f6a82f142f439648a5f4f
Alex <alex.sanders@ukr.net>
2021-05-19 13:03:07 +0300

    Update smoothScroll.js

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,14 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
 
-    smoothScrollElems.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
+smoothScrollElems.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
 
-            const id = link.getAttribute('href').substring(1);
-            document.getElementById(id).scrollIntoView({
-                behavior: "smooth"
-            });
+        const id = link.getAttribute('href').substring(1);
+        document.getElementById(id).scrollIntoView({
+            behavior: "smooth"
         });
     });
 });
